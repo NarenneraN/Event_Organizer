e7892ca22b61630cdeb0a7d32392a3a193cebdfb
NarenneraN <narendhiran.pugazhendhi@gmail.com>
2021-09-22 17:40:53 +0530

    😄

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const app=express();
-
+//Respond to req
 app.get("/",function(req,res){
   
 });
+//listen Module
 app.listen(3000,function(){
   console.log("server Started!");
 });
