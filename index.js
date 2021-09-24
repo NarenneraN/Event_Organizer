88aa4596f2e68605b4b68f96b06f14f7bdff036d
NarenneraN <narendhiran.pugazhendhi@gmail.com>
2021-09-24 10:16:15 +0530

    update js (Send file on req)

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const app=express();
+app.use(express.static('public'));
 //Respond to req
 app.get("/",function(req,res){
-  
+  res.sendFile(__dirname+"/index.html");
 });
 //listen Module
 app.listen(3000,function(){
