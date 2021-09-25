4b8e67ca4f108c6be46b13424e1ef781571a6fd9
NarenneraN <narendhiran.pugazhendhi@gmail.com>
2021-09-25 12:48:14 +0530

    finish v1.0

--- public/event.js
+++ public/event.js
@@ -1,4 +1,9 @@
 
 function change_content(event_no){
 location.href=`/event/${event_no}`;
+currr_active=event_no;
+document.getElementById("arrow_"+curr_active).style.left=2+'px';
+}
+function deleteit(event_no){
+location.href=`/event_d/${event_no}`;
 }
